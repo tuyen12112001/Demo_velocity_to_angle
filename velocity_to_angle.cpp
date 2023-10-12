26c92a3d2e59e844eebfef6b80bac5947c437fdc
tuyen12112001 <tuyen10a2bdq@gmail.com>
2023-10-12 11:13:30 +0900

    init project

---
+++ velocity_to_angle.cpp
@@ -0,0 +1,19 @@
+﻿#include <velocity_to_anlge.h>
+int main() {
+    // Giả sử vận tốc và thời gian là:
+    double velocity = 1.0; // RPM
+    double time = 1.0; // s
+
+    while (true) {
+        // Chuyển đổi vận tốc sang góc
+        double angle = velocitytoangle(velocity, time);
+
+        // In giá trị góc ra màn hình
+        std::cout << "Góc: " << angle << " độ" << std::endl;
+
+        // 1秒待ちます
+        sleep(1);
+    }
+
+    return 0;
+}
---
+++ velocity_to_angle.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+// 速度を角度に変換する関数
+void velocitytoangle(double velocity, double time) {
+    // 速度を RPM から rad/s に変換します
+    double velocity_rad_s = velocity * 2 * M_PI / 60;
+
+    // 角度を計算する
+    double angle_rad = velocity_rad_s * time;
+
+    // 角度をラジアンから度に変換します
+    double angle_deg = angle_rad * 180 / M_PI;
+
+    return angle_deg;
+}
